21f59ea1a19b86c39c72ee1960878615901765ac
will258012 <will258012@gmail.com>
2023-12-23 09:22:06 +0800

    Update Program.cs
    - 增加DPI感知，以修复自动缩放导致的分辨率不准等问题

--- Program.cs
+++ Program.cs
@@ -10,12 +10,15 @@ namespace DashboardPositioner
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
         [STAThread]
         static void Main()
         {
+            SetProcessDPIAware();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new mainForm());
+            Application.Run(new mainForm());   
         }
     }
 }
